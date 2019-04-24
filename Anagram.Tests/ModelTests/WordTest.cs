7971fcc00328eec1de895e2c7be2cfd83e9907eb
Colton Lacey and Zsuzsanna Mangu <colton@>
2019-04-24 15:51:55 -0700

    add Sort function to Word.cs file and run test successfully.

--- Anagram.Tests/ModelTests/WordTest.cs
+++ Anagram.Tests/ModelTests/WordTest.cs
@@ -1,25 +1,65 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Anagram.Models;
+using System;
+using System.Collections.Generic;
 
 namespace Anagram.Tests
 {
   [TestClass]
-  public class AnagramTest
-  {
+ public class ItemTest : IDisposable
+ {
+
+   public void Dispose()
+   {
+     //Act
+     Word.ClearAll();
+   }
+
+
     [TestMethod]
-    public void IsThisAWord_WordEntered_String()
+    public void IsThereAnAnagram_GetWordThatMatches_True()
     {
-      AnagramClass testAnagram = new AnagramClass();
-      Assert.AreEqual("bread", testAnagram.IsThisAWord("bread"));
+      //Arrange
+      Word newWord = new Word ("test");
+
+      //Act
+      // string output = newWord.IsThereAnAnagram();
+
+      //Assert
+
+      Assert.AreEqual (true, Word.IsThereAnAnagram("bread", "beard"));
     }
-    [TestMethod]
-    public void IsThisAListOfWords_ListEntered_True()
-    {
-      AnagramClass testAnagram = new AnagramClass();
-      Assert.AreEqual(wordList, testAnagram.IsThisAListOfWords("bear, beard, break"));
-    }
 
-  }
+    // [TestMethod]
+    // public void IsThereAnAnagram_GetWordThatMatches_False()
+    // {
+    //   //Arrange
+    //   Word newWord = new Word ("test");
+    //
+    //   //Act
+    //   // string output = newWord.IsThereAnAnagram();
+    //
+    //   //Assert
+    //
+    //   Assert.AreEqual (true, Word.IsThereAnAnagram("bread", "beard"));
+    // }
 
 
+
+
+  // [TestMethod]
+  // public void Word_GetAll_String()
+  // {
+  //   //Arrange
+  //   Word newWord = new Word ("test");
+  //
+  //   //Act
+  //   string output = newWord.GetAll();
+  //
+  //   //Assert
+  //   Assert.AreEqual (output, true)
+  // }
+
+
+}
 }
--- Anagram/Models/Word.cs
+++ Anagram/Models/Word.cs
@@ -4,28 +4,50 @@ using System.Collections.Generic;
 namespace Anagram.Models
 {
 
-  public class AnagramClass
+  public class Word
   {
-    public string IsThisAWord(string word)
-    {
-      return "bread";
-    }
-    public List<string> IsThisAListOfWords(string list)
-    {
-      List<string> wordList = new List<string>();
+    private string _word;
+    private static List<Word> _instances = new List<Word> {};
 
-      foreach (string listItem in wordList)
-      {
-        wordList.Add(IsThisAListOfWords);
-      {
-        return wordList;
-      }
-      }
+    public Word (string word)
+    {
+      _word = word;
+      _instances.Add(this);
     }
 
+    public string GetWord()
+    {
+      return _word;
+    }
 
+    public static List<Word> GetAll()
+    {
+      return _instances;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
 
+    public static bool IsThereAnAnagram(string word,   string check)
+    {
+      char[] wordArray = word.ToCharArray();
+      char[] checkArray = check.ToCharArray();
 
+      Array.Sort(wordArray);
+      Array.Sort(checkArray);
+
+      string wordString = String.Join("", wordArray);
+      string checkString = String.Join("", checkArray);
+
+        if (wordString == checkString)
+        {
+          return true;
+        }
+        else {
+          return false;
+        }
+
+    }
   }
-
 }
